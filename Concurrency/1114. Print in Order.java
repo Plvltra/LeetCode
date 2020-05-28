b851474abfe8ca2dcc6206ad3794823856d0e20d
Plvltra <331079072@qq.com>
2020-05-28 15:53:18 +0800

    2020_05_27

---
+++ Concurrency/1114. Print in Order.java
@@ -0,0 +1,30 @@
+class Foo {
+    Semaphore s1;
+    Semaphore s2;
+    public Foo() {
+        s1 = new Semaphore(0);
+        s2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        s1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        s1.acquire();
+        printSecond.run();
+        s2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        s2.acquire();
+        printThird.run();
+    }
+}
